de4075754c26b38c511a3790361bb058069064cf
Fabian Vu <Fabian.Vu@hhu.de>
2018-12-11 16:59:37 +0100

    Rename benchmarks and remove obsolete benchmarks

---
+++ benchmarks/java/set/Clojure_Set_From_Java/RangeElementOfBig.java
@@ -0,0 +1,40 @@
+import de.hhu.stups.btypes.BSet;
+import de.hhu.stups.btypes.BInteger;
+import de.hhu.stups.btypes.BBoolean;
+
+public class RangeElementOfBig {
+
+
+
+
+    private BInteger counter;
+
+    private boolean initialized = false;
+
+    public void initialize() {
+        if(initialized) {
+            throw new RuntimeException("Machine is already initialized");
+        }
+        counter = (BInteger) new BInteger(0);
+        initialized = true;
+    }
+
+    public void simulate() {
+        if(!initialized) {
+            throw new RuntimeException("Machine was not initialized");
+        }
+        while((counter.less(new BInteger(10000)).and(BSet.range(new BInteger(1),new BInteger(25000)).elementOf(new BInteger(25000)))).booleanValue()) {
+            counter = (BInteger) counter.plus(new BInteger(1));
+        }
+    }
+
+    public static void main(String[] args) {
+        RangeElementOfBig exec = new RangeElementOfBig();
+        exec.initialize();
+        long start = System.nanoTime();
+        exec.simulate();
+        long end = System.nanoTime();
+        System.out.println(exec.getClass().toString() + " Execution: " + (end - start));
+    }
+
+}

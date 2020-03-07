d5dde8eebef72f4a3cd9e1a08b2b67d7f1b01273
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2020-03-07 14:45:00 +0100

    Add Benchmarks for files generated from Lustre

--- src/test/java/de/hhu/stups/codegenerator/cpp/TestLustreBenchmarks.java
+++ src/test/java/de/hhu/stups/codegenerator/cpp/TestLustreBenchmarks.java
@@ -5,128 +5,38 @@ import org.junit.Test;
 public class TestLustreBenchmarks extends TestCpp {
 
 	@Test
-	public void testAnd() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_and_exec", "microbenchmarks_and_exec", "microbenchmarks_and_execAddition.stcpp", false);
+	public void testLift() throws Exception {
+		testCpp("lustre_benchmarks/Lift_main_exec", "Lift_main_exec", "Lift_main_execAddition.stcpp", false);
 	}
 
 	@Test
-	public void testCurrentBoolean() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_current_boolean_exec", "microbenchmarks_current_boolean_exec", "microbenchmarks_current_boolean_execAddition.stcpp", false);
+	public void testLiftVerif() throws Exception {
+		testCpp("lustre_benchmarks/Lift_verif_exec", "Lift_verif_exec", "Lift_verif_execAddition.stcpp", false);
 	}
 
 	@Test
-	public void testCurrentInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_current_integer_exec", "microbenchmarks_current_integer_exec", "microbenchmarks_current_integer_execAddition.stcpp", false);
+	public void testSLOW_TIME_STABLE() throws Exception {
+		testCpp("lustre_benchmarks/SLOW_TIME_STABLE_exec", "SLOW_TIME_STABLE_exec", "SLOW_TIME_STABLE_execAddition.stcpp", false);
 	}
 
 	@Test
-	public void testDivideInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_divide_integer_exec", "microbenchmarks_divide_integer_exec", "microbenchmarks_divide_integer_execAddition.stcpp", false);
+	public void testSpeed() throws Exception {
+		testCpp("lustre_benchmarks/speed_main_exec", "speed_main_exec", "speed_main_execAddition.stcpp", false);
 	}
 
 	@Test
-	public void testEqualBoolean() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_equal_boolean_exec", "microbenchmarks_equal_boolean_exec", "microbenchmarks_equal_boolean_execAddition.stcpp", false);
+	public void testSpeedVerif() throws Exception {
+		testCpp("lustre_benchmarks/speed_verif_exec", "speed_verif_exec", "speed_verif_execAddition.stcpp", false);
 	}
 
 	@Test
-	public void testEqualInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_equal_integer_exec", "microbenchmarks_equal_integer_exec", "microbenchmarks_equal_integer_execAddition.stcpp", false);
+	public void testUMS() throws Exception {
+		testCpp("lustre_benchmarks/UMS_main_exec", "UMS_main_exec", "UMS_main_execAddition.stcpp", false);
 	}
 
 	@Test
-	public void testFbyBoolean() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_fby_boolean_exec", "microbenchmarks_fby_boolean_exec", "microbenchmarks_fby_boolean_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testFbyInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_fby_integer_exec", "microbenchmarks_fby_integer_exec", "microbenchmarks_fby_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testGreaterEqualInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_greater_equal_integer_exec", "microbenchmarks_greater_equal_integer_exec", "microbenchmarks_greater_equal_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testGreaterInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_greater_integer_exec", "microbenchmarks_greater_integer_exec", "microbenchmarks_greater_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testIteBoolean() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_ite_boolean_exec", "microbenchmarks_ite_boolean_exec", "microbenchmarks_ite_boolean_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testIteInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_ite_integer_exec", "microbenchmarks_ite_integer_exec", "microbenchmarks_ite_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testLessEqualInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_less_equal_integer_exec", "microbenchmarks_less_equal_integer_exec", "microbenchmarks_less_equal_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testLessInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_less_integer_exec", "microbenchmarks_less_integer_exec", "microbenchmarks_less_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testMinusInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_minus_integer_exec", "microbenchmarks_minus_integer_exec", "microbenchmarks_minus_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testMultiplyInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_multiply_integer_exec", "microbenchmarks_multiply_integer_exec", "microbenchmarks_multiply_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testNot() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_not_exec", "microbenchmarks_not_exec", "microbenchmarks_not_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testOr() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_or_exec", "microbenchmarks_or_exec", "microbenchmarks_or_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testPlusInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_plus_integer_exec", "microbenchmarks_plus_integer_exec", "microbenchmarks_plus_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testPreBoolean() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_pre_boolean_exec", "microbenchmarks_pre_boolean_exec", "microbenchmarks_pre_boolean_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testPreInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_pre_integer_exec", "microbenchmarks_pre_integer_exec", "microbenchmarks_pre_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testUnequalBoolean() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_unequal_boolean_exec", "microbenchmarks_unequal_boolean_exec", "microbenchmarks_unequal_boolean_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testUnequalInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_unequal_integer_exec", "microbenchmarks_unequal_integer_exec", "microbenchmarks_unequal_integer_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testWhenBoolean() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_when_boolean_exec", "microbenchmarks_when_boolean_exec", "microbenchmarks_when_boolean_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testWhenInteger() throws Exception {
-		testCpp("lustre_microbenchmarks/microbenchmarks_when_integer_exec", "microbenchmarks_when_integer_exec", "microbenchmarks_when_integer_execAddition.stcpp", false);
+	public void testUMSVerif() throws Exception {
+		testCpp("lustre_benchmarks/UMS_verif_exec", "UMS_verif_exec", "UMS_verif_execAddition.stcpp", false);
 	}
 
 }

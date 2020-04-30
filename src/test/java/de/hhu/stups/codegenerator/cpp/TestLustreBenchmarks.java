690140a8c0050ec5a00cf313f4bc60452cabfdbc
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2020-04-30 20:51:24 +0200

    Update tests for B files generated from Lustre

--- src/test/java/de/hhu/stups/codegenerator/cpp/TestLustreBenchmarks.java
+++ src/test/java/de/hhu/stups/codegenerator/cpp/TestLustreBenchmarks.java
@@ -6,37 +6,37 @@ public class TestLustreBenchmarks extends TestCpp {
 
 	@Test
 	public void testLift() throws Exception {
-		testCpp("lustre_benchmarks/Lift_main_exec", "Lift_main_exec", "Lift_main_execAddition.stcpp", false);
-	}
-
-	@Test
-	public void testLiftVerif() throws Exception {
-		testCpp("lustre_benchmarks/Lift_verif_exec", "Lift_verif_exec", "Lift_verif_execAddition.stcpp", false);
+		testCpp("lustre_benchmarks/M_Lift_exec", "M_Lift_exec", "M_Lift_execAddition.stcpp", false);
 	}
 
 	@Test
 	public void testSLOW_TIME_STABLE() throws Exception {
-		testCpp("lustre_benchmarks/SLOW_TIME_STABLE_exec", "SLOW_TIME_STABLE_exec", "SLOW_TIME_STABLE_execAddition.stcpp", false);
+		testCpp("lustre_benchmarks/M_SLOW_TIME_STABLE_exec", "M_SLOW_TIME_STABLE_exec", "M_SLOW_TIME_STABLE_execAddition.stcpp", false);
 	}
 
 	@Test
 	public void testSpeed() throws Exception {
-		testCpp("lustre_benchmarks/speed_main_exec", "speed_main_exec", "speed_main_execAddition.stcpp", false);
+		testCpp("lustre_benchmarks/M_speed_main_exec", "M_speed_main_exec", "M_speed_main_execAddition.stcpp", false);
 	}
 
 	@Test
 	public void testSpeedVerif() throws Exception {
-		testCpp("lustre_benchmarks/speed_verif_exec", "speed_verif_exec", "speed_verif_execAddition.stcpp", false);
+		testCpp("lustre_benchmarks/M_speed_verif_exec", "M_speed_verif_exec", "M_speed_verif_execAddition.stcpp", false);
 	}
 
 	@Test
 	public void testUMS() throws Exception {
-		testCpp("lustre_benchmarks/UMS_main_exec", "UMS_main_exec", "UMS_main_execAddition.stcpp", false);
+		testCpp("lustre_benchmarks/M_UMS_main_exec", "M_UMS_main_exec", "M_UMS_main_execAddition.stcpp", false);
 	}
 
 	@Test
 	public void testUMSVerif() throws Exception {
-		testCpp("lustre_benchmarks/UMS_verif_exec", "UMS_verif_exec", "UMS_verif_execAddition.stcpp", false);
+		testCpp("lustre_benchmarks/M_UMS_verif_exec", "M_UMS_verif_exec", "M_UMS_verif_execAddition.stcpp", false);
 	}
+
+    @Test
+    public void testPilotFlying() throws Exception {
+        testCpp("lustre_benchmarks/M_pilot_flying_exec", "M_pilot_flying_exec", "M_pilot_flying_execAddition.stcpp", false);
+    }
 
 }
--- src/test/java/de/hhu/stups/codegenerator/java/TestLustreBenchmarks.java
+++ src/test/java/de/hhu/stups/codegenerator/java/TestLustreBenchmarks.java
@@ -6,37 +6,37 @@ public class TestLustreBenchmarks extends TestJava {
 
 	@Test
 	public void testLift() throws Exception {
-		testJava("lustre_benchmarks/Lift_main_exec", "Lift_main_exec", "Lift_main_execAddition.stjava", false);
-	}
-
-	@Test
-	public void testLiftVerif() throws Exception {
-		testJava("lustre_benchmarks/Lift_verif_exec", "Lift_verif_exec", "Lift_verif_execAddition.stjava", false);
+		testJava("lustre_benchmarks/M_Lift_exec", "M_Lift_exec", "M_Lift_execAddition.stjava", false);
 	}
 
 	@Test
 	public void testSLOW_TIME_STABLE() throws Exception {
-		testJava("lustre_benchmarks/SLOW_TIME_STABLE_exec", "SLOW_TIME_STABLE_exec", "SLOW_TIME_STABLE_execAddition.stjava", false);
+		testJava("lustre_benchmarks/M_SLOW_TIME_STABLE_exec", "M_SLOW_TIME_STABLE_exec", "M_SLOW_TIME_STABLE_execAddition.stjava", false);
 	}
 
 	@Test
 	public void testSpeed() throws Exception {
-		testJava("lustre_benchmarks/speed_main_exec", "speed_main_exec", "speed_main_execAddition.stjava", false);
+		testJava("lustre_benchmarks/M_speed_main_exec", "M_speed_main_exec", "M_speed_main_execAddition.stjava", false);
 	}
 
 	@Test
 	public void testSpeedVerif() throws Exception {
-		testJava("lustre_benchmarks/speed_verif_exec", "speed_verif_exec", "speed_verif_execAddition.stjava", false);
+		testJava("lustre_benchmarks/M_speed_verif_exec", "M_speed_verif_exec", "M_speed_verif_execAddition.stjava", false);
 	}
 
 	@Test
 	public void testUMS() throws Exception {
-		testJava("lustre_benchmarks/UMS_main_exec", "UMS_main_exec", "UMS_main_execAddition.stjava", false);
+		testJava("lustre_benchmarks/M_UMS_main_exec", "M_UMS_main_exec", "M_UMS_main_execAddition.stjava", false);
 	}
 
 	@Test
 	public void testUMSVerif() throws Exception {
-		testJava("lustre_benchmarks/UMS_verif_exec", "UMS_verif_exec", "UMS_verif_execAddition.stjava", false);
+		testJava("lustre_benchmarks/M_UMS_verif_exec", "M_UMS_verif_exec", "M_UMS_verif_execAddition.stjava", false);
+	}
+
+	@Test
+	public void testPilotFlying() throws Exception {
+		testJava("lustre_benchmarks/M_pilot_flying_exec", "M_pilot_flying_exec", "M_pilot_flying_execAddition.stjava", false);
 	}
 
 }

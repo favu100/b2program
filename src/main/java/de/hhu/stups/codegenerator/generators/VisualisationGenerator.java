8befe2f620d2630972a1718572e6262da0a9129d
Christopher Happe <Christopher.Happe@uni-duesseldorf.de>
2021-10-06 16:18:04 +0200

    Added visualisation for Variables and Constants

--- src/main/java/de/hhu/stups/codegenerator/generators/VisualisationGenerator.java
+++ src/main/java/de/hhu/stups/codegenerator/generators/VisualisationGenerator.java
@@ -2,11 +2,14 @@ package de.hhu.stups.codegenerator.generators;
 
 import java.util.*;
 import java.util.stream.Collectors;
+
 import de.hhu.stups.codegenerator.handlers.TemplateHandler;
 import de.hhu.stups.codegenerator.json.visb.VisBEvent;
 import de.hhu.stups.codegenerator.json.visb.VisBItem;
 import de.hhu.stups.codegenerator.json.visb.VisBProject;
 import de.prob.parser.ast.nodes.DeclarationNode;
+import de.prob.parser.ast.nodes.MachineNode;
+import de.prob.parser.ast.nodes.MachineReferenceNode;
 import de.prob.parser.ast.nodes.OperationNode;
 import de.prob.parser.ast.types.BType;
 import de.prob.parser.ast.types.EnumeratedSetElementType;
@@ -48,10 +51,88 @@ public class VisualisationGenerator{
     TemplateHandler.add(visualisation, "visualUpdates", visBProject.getVisualisation().getVisBItems().stream().map((VisBItem item) -> generateVisualUpdate(item, visBProject)).collect(Collectors.toSet()));
     TemplateHandler.add(visualisation, "machineEnums", expressionGenerator.getNameHandler().getEnumTypes().keySet().stream().filter((String machineEnum) -> expressionGenerator.getNameHandler().getEnumToMachine().get(machineEnum).equals(visBProject.getProject().getMainMachine().getName())).collect(Collectors.toSet()));
     TemplateHandler.add(visualisation, "importedEnums", importGenerator.getImportedEnums());
+    TemplateHandler.add(visualisation, "includedMachines", generateIncludedMachines(visBProject.getProject().getMainMachine()));
+    TemplateHandler.add(visualisation, "variables", generateVariables(visBProject.getProject().getMainMachine()));
+    TemplateHandler.add(visualisation, "constants", generateConstants(visBProject.getProject().getMainMachine()));
+    TemplateHandler.add(visualisation, "variableUpdates", generateVariableUpdates(visBProject.getProject().getMainMachine()));
     //Adding imports last to ensure all needed types are imported.
     TemplateHandler.add(visualisation, "imports", importGenerator.getImportedTypes().stream().map(this::generateVisualisationImport).collect(Collectors.toSet()));
 
     return visualisation.render();
+  }
+
+  private List<String> generateIncludedMachines(MachineNode mainMachine) {
+    List<List<String>> includedMachines = new ArrayList<>();
+    for (MachineReferenceNode included : mainMachine.getMachineReferences()) {
+      List<List<String>> nestedIncludes = generateCurrentIncludedMachine(included.getMachineNode());
+      nestedIncludes.forEach(includeList -> includeList.add(0, visualisationGroup.getInstanceOf("initialMachineVar").render()));
+      includedMachines.addAll(nestedIncludes);
+    }
+    List<String> savedMachines = new ArrayList<>();
+    includedMachines.forEach((List<String> includedList) -> {
+      ST mapIncludedMachineName = visualisationGroup.getInstanceOf("mapIncludedMachineName");
+      TemplateHandler.add(mapIncludedMachineName, "machineName", includedList.get(includedList.size()-1));
+      String pathFromMain = includedList.stream().reduce((String accPath, String currentMachineNode) -> {
+        ST generatedPath = visualisationGroup.getInstanceOf("getMachineFromMachine");
+        TemplateHandler.add(generatedPath, "machine", accPath);
+        TemplateHandler.add(generatedPath, "includedMachine", currentMachineNode);
+        return generatedPath.render();
+      }).get();
+      TemplateHandler.add(mapIncludedMachineName, "pathFromMainMachine", pathFromMain);
+      savedMachines.add(mapIncludedMachineName.render());
+    });
+    return savedMachines;
+  }
+
+  private List<List<String>> generateCurrentIncludedMachine(MachineNode includedMachine) {
+    List<List<String>> includedMachines = new ArrayList<>();
+    for (MachineReferenceNode included : includedMachine.getMachineReferences()) {
+      List<List<String>> nestedIncludes = generateCurrentIncludedMachine(included.getMachineNode());
+      nestedIncludes.forEach(includeList -> includeList.add(0, includedMachine.getName()));
+      includedMachines.addAll(nestedIncludes);
+    }
+    List<String> includedMachinePath = new ArrayList<>();
+    includedMachinePath.add(includedMachine.getName());
+    includedMachines.add(0, includedMachinePath);
+    return includedMachines;
+  }
+
+  private List<String> generateVariables(MachineNode machine) {
+    List<String> ownVariables = machine.getVariables().stream().map(var -> generateVariable(var, machine)).collect(Collectors.toList());
+    List<String> otherVariables = new ArrayList<>();
+
+    for (MachineReferenceNode includedMachine : machine.getMachineReferences()) {
+      List<String> strings = generateVariables(includedMachine.getMachineNode());
+      otherVariables.addAll(strings);
+    }
+    ownVariables.addAll(otherVariables);
+    return ownVariables;
+  }
+
+  private String generateVariable(DeclarationNode var, MachineNode machine) {
+    ST variable = visualisationGroup.getInstanceOf("variable");
+    TemplateHandler.add(variable, "var", var.getName());
+    TemplateHandler.add(variable, "machineName", machine.getName());
+    return variable.render();
+  }
+
+  private List<String> generateConstants(MachineNode machine) {
+    List<String> ownConstants = machine.getConstants().stream().map(var -> generateConstant(var, machine)).collect(Collectors.toList());
+    List<String> otherConstants = new ArrayList<>();
+
+    for (MachineReferenceNode includedMachine : machine.getMachineReferences()) {
+      List<String> strings = generateConstants(includedMachine.getMachineNode());
+      otherConstants.addAll(strings);
+    }
+    ownConstants.addAll(otherConstants);
+    return ownConstants;
+  }
+
+  private String generateConstant(DeclarationNode var, MachineNode machine) {
+    ST constant = visualisationGroup.getInstanceOf("constant");
+    TemplateHandler.add(constant, "var", var.getName());
+    TemplateHandler.add(constant, "machineName", machine.getName());
+    return constant.render();
   }
 
   public String generateVisualUpdate(VisBItem item, VisBProject visBProject) {
@@ -61,6 +142,25 @@ public class VisualisationGenerator{
     TemplateHandler.add(visualUpdate, "expression", expressionGenerator.visitExprNode(item.getExprNode()).replaceAll("this.", "_machine.").replaceAll("_machine." + visBProject.getProject().getMainMachine().getName() + ".", "_machine.")+ ".getValue()");
 
     return visualUpdate.render();
+  }
+
+  private List<String> generateVariableUpdates(MachineNode machine) {
+    List<String> ownVariables = machine.getVariables().stream().map(var -> generateVariableUpdate(var, machine)).collect(Collectors.toList());
+    List<String> otherVariables = new ArrayList<>();
+
+    for (MachineReferenceNode includedMachine : machine.getMachineReferences()) {
+      List<String> strings = generateVariableUpdates(includedMachine.getMachineNode());
+      otherVariables.addAll(strings);
+    }
+    ownVariables.addAll(otherVariables);
+    return ownVariables;
+  }
+
+  private String generateVariableUpdate(DeclarationNode var, MachineNode machine) {
+    ST variable = visualisationGroup.getInstanceOf("variableUpdate");
+    TemplateHandler.add(variable, "variable", var.getName());
+    TemplateHandler.add(variable, "machineName", machine.getName());
+    return variable.render();
   }
 
   public String generateEventParameter(DeclarationNode param, String eventName) {

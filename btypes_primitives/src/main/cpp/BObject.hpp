353be9f9325e631ebbef794aa0db96888447a5ae
Fabian Vu <Fabian.Vu@hhu.de>
2022-07-07 11:43:44 +0200

    Add comments in B types primitives in C++

--- btypes_primitives/src/main/cpp/BObject.hpp
+++ btypes_primitives/src/main/cpp/BObject.hpp
@@ -6,9 +6,9 @@ class BObject {
 
     public:
 
-        friend bool operator !=(const BObject& p1, const BObject& p2) {return true;}
+        friend bool operator !=(const BObject& p1, const BObject& p2) {return true;} // This is only used for untyped empty sets. This function is overridden in sub-classes.
 
-        friend bool operator ==(const BObject& p1, const BObject& p2) {return false;}
+        friend bool operator ==(const BObject& p1, const BObject& p2) {return false;} // This is only used for untyped empty sets. This function is overridden in sub-classes.
 
         virtual int hashCode() const {
             return 0;

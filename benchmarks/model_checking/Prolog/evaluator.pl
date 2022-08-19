04be0aa47ec2fc1c9cb523a9767a14123eecd8ea
dobra101 <dobra101@uni-duesseldorf.de>
2022-08-19 15:42:58 +0100

    added prolog benchmark

--- benchmarks/model_checking/Prolog/evaluator.pl
+++ benchmarks/model_checking/Prolog/evaluator.pl
@@ -34,24 +34,24 @@
 :- use_module(library(sets)).
 
 elementOf(State, X, List) :-
-    (var(X); (nonvar(X), \+ avl_fetch(X, State))),
-    is_list(List),
+    (var(X); (nonvar(X), \+ avl:avl_fetch(X, State))),
+    lists:is_list(List),
     member(X, List),
     !.
 elementOf(State, X, Key) :-
     var(X),
-    avl_fetch(Key, State, List),
+    avl:avl_fetch(Key, State, List),
     member(X, List),
     !.
 elementOf(State, Key, interval(A, B)) :-
     nonvar(Key),
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     greaterEqual(Value, A),
     lessEqual(Value, B),
     !.
 elementOf(State, Key, List) :-
     nonvar(Key),
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     member(Value, List),
     !.
 
@@ -60,7 +60,7 @@ notElementOf(S, A, B) :-
     !.
 
 isNatural(State, Key) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     Value > 0,
     integer(Value),
     !.
@@ -70,11 +70,11 @@ isNatural(State, Key) :-
 cartesianProduct(_, _, notImplemented).
 
 pow(_, List, Pow) :-
-    setof(P, subseq0(List, P), Pow),
+    sets:setof(P, lists:subseq0(List, P), Pow),
     !.
 
 range(State, Key, Range) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     range(Value, Range),
     !.
 
@@ -84,7 +84,7 @@ range([(R/_) | Tail], [R | Range]) :-
     !.
 
 domain(State, Key, Domain) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     domain(Value, Domain), !.
 
 domain([], []) :- !.
@@ -93,58 +93,58 @@ domain([(_/D) | Tail], [D | Domain]) :-
     !.
 
 card(State, Key, Card) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     length(Value, Card), !.
 
 less(A, B) :- A < B.
 less(State, Key, X) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     less(Value, X).
 
 greater(A, B) :- A > B.
 greater(State, Key, X) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     greater(Value, X).
 
 lessEqual(A, B) :- A =< B.
 lessEqual(State, Key, X) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     lessEqual(Value, X).
 lessEqual(_, A, B) :- lessEqual(A, B).
 
 greaterEqual(A, B) :- A >= B.
 greaterEqual(State, Key, X) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     greaterEqual(Value, X).
 
 equal(State, Key, Value) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     !.
 
 unequal(State, Key, Value) :-
-    avl_fetch(Key, State, X),
+    avl:avl_fetch(Key, State, X),
     X \= Value,
     !.
 
 plus(State, Key, X, NewValue) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     NewValue is Value + X.
 
 minus(State, Key, X, NewValue) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     NewValue is Value - X.
 
 mult(State, Key, X, NewValue) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     NewValue is Value * X.
 
 div(State, Key, X, NewValue) :-
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     NewValue is Value / X.
 
 difference(State, Key, Set, Diff) :-
     nonvar(Key),
-    avl_fetch(Key, State, Value),
+    avl:avl_fetch(Key, State, Value),
     findall(X,
         (member(X, Value), \+ member(X, Set)),
         Diff),
@@ -152,49 +152,49 @@ difference(State, Key, Set, Diff) :-
 
 union(State, Key, Set, Union) :-
     nonvar(Key),
-    is_list(Set),
-    avl_fetch(Key, State, Value),
-    union(Value, Set, Union),
+    lists:is_list(Set),
+    avl:avl_fetch(Key, State, Value),
+    sets:union(Value, Set, Union),
     !.
 union(State, Set, Key, Union) :-
     nonvar(Key),
-    is_list(Set),
-    avl_fetch(Key, State, Value),
-    union(Value, Set, Union),
+    lists:is_list(Set),
+    avl:avl_fetch(Key, State, Value),
+    sets:union(Value, Set, Union),
     !.
 union(State, Key1, Key2, Union) :-
     nonvar(Key1),
     nonvar(Key2),
-    avl_fetch(Key1, State, Value1),
-    avl_fetch(Key2, State, Value2),
-    union(Value1, Value2, Union),
+    avl:avl_fetch(Key1, State, Value1),
+    avl:avl_fetch(Key2, State, Value2),
+    sets:union(Value1, Value2, Union),
     !.
 
 intersect(State, Key, Set, Inter) :-
     nonvar(Key),
-    is_list(Set),
-    avl_fetch(Key, State, Value),
-    intersection(Value, Set, Inter),
+    lists:is_list(Set),
+    avl:avl_fetch(Key, State, Value),
+    sets:intersection(Value, Set, Inter),
     !.
 intersect(State, Set, Key, Inter) :-
     nonvar(Key),
-    is_list(Set),
-    avl_fetch(Key, State, Value),
-    intersection(Value, Set, Inter),
+    lists:is_list(Set),
+    avl:avl_fetch(Key, State, Value),
+    sets:intersection(Value, Set, Inter),
     !.
 intersect(State, Key1, Key2, Inter) :-
     nonvar(Key1),
     nonvar(Key2),
-    avl_fetch(Key1, State, Value1),
-    avl_fetch(Key2, State, Value2),
-    intersection(Value1, Value2, Inter),
+    avl:avl_fetch(Key1, State, Value1),
+    avl:avl_fetch(Key2, State, Value2),
+    sets:intersection(Value1, Value2, Inter),
     !.
 
 subset(State, Key, Set) :-
     nonvar(Key),
-    is_list(Set);
-    avl_fetch(Key, State, Value),
-    subseq0(Value, Set),
+    lists:is_list(Set);
+    avl:avl_fetch(Key, State, Value),
+    lists:subseq0(Value, Set),
     !.
 
 b_member(Val, interval(Val, Val)) :- !.
@@ -212,11 +212,10 @@ next(interval(A, B), A, interval(A1, B)) :-
 
 
 % AVL TREE
-empty(T) :- empty_avl(T).
+empty(T) :- avl:empty_avl(T).
 
 update(Key, Value, AVL, NewAVL) :-
-    avl_store(Key, AVL, Value, NewAVL).
+    avl:avl_store(Key, AVL, Value, NewAVL).
 
 avl2List(Tree, List) :-
-    avl_to_list(Tree, List).
-
+    avl:avl_to_list(Tree, List).

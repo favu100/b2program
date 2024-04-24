5449956be88391e9d8926e4d50dc97aec8f74316
Fabian Vu <Fabian.Vu@hhu.de>
2024-04-24 22:07:00 +0200

    Add Python script to compute median for runtime and memory for Java, C++, and JavaScript

---
+++ benchmarks/model_checking/runtimes_exec.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def calculate_statistics(data, index_machine, threads, threads_index_present, caching):
+    machines = {}
+    for line in data:
+        split_line = line.split()
+        if len(split_line) == 0:
+            continue
+        if threads_index_present:
+            if threads != int(line.split()[-4]):
+                continue
+        if caching != line.split()[-3]:
+            continue
+        machine_name = line.split()[index_machine]
+        time_seconds = float(line.split()[-2].split(':')[0]) * 60 + float(line.split()[-2].split(':')[1])
+        memory_mb = round(float(line.split()[-1]) / 1000, 2)
+
+        if machine_name not in machines:
+            machines[machine_name] = {'time': [], 'memory': []}
+
+        machines[machine_name]['time'].append(time_seconds)
+        machines[machine_name]['memory'].append(memory_mb)
+
+    for machine, values in machines.items():
+        time_avg = round(np.median(values['time']), 2)
+        memory_avg = round(np.median(values['memory']), 2)
+        print(f"Machine: {machine}, Average Time: {time_avg} seconds, Average Memory: {memory_avg} MB")
+
+with open('Java/runtimes.txt', 'r') as file:
+    data_java = file.readlines()
+
+with open('C++/runtimes.txt', 'r') as file:
+    data_cpp = file.readlines()
+
+with open('JS/runtimes.txt', 'r') as file:
+    data_js = file.readlines()
+
+print('Java (Single-threaded, No Caching):')
+calculate_statistics(data_java, 3, 1, True, "false")
+print('----------------------------')
+print('C++ (Single-threaded, No Caching):')
+calculate_statistics(data_cpp, 0, 1, True, "false")
+print('----------------------------')
+print('JS (Single-threaded, No Caching):')
+calculate_statistics(data_js, 1, 1, False, "false")
+print('----------------------------')
+
+print('Java (Single-threaded, Caching):')
+calculate_statistics(data_java, 3, 1, True, "true")
+print('----------------------------')
+print('C++ (Single-threaded, Caching):')
+calculate_statistics(data_cpp, 0, 1, True, "true")
+print('----------------------------')
+print('JS (Single-threaded, Caching):')
+calculate_statistics(data_js, 1, 1, False, "true")
+print('----------------------------')
+
+
+print('Java (Multi-threaded, No Caching):')
+calculate_statistics(data_java, 3, 6, True, "false")
+print('----------------------------')
+print('C++ (Multi-threaded, No Caching):')
+calculate_statistics(data_cpp, 0, 6, True, "false")
+print('----------------------------')
+
+print('Java (Multi-threaded, Caching):')
+calculate_statistics(data_java, 3, 6, True, "true")
+print('----------------------------')
+print('C++ (Multi-threaded, Caching):')
+calculate_statistics(data_cpp, 0, 6, True, "true")
+print('----------------------------')

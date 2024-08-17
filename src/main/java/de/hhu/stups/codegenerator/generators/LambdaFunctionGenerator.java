c210261761161fa5fb670afde5936ca6f7841afa
Fabian Vu <Fabian.Vu@hhu.de>
2024-08-17 13:58:31 +0200

    Add TODOs

--- src/main/java/de/hhu/stups/codegenerator/generators/LambdaFunctionGenerator.java
+++ src/main/java/de/hhu/stups/codegenerator/generators/LambdaFunctionGenerator.java
@@ -5,10 +5,44 @@ import de.prob.parser.ast.nodes.DeclarationNode;
 import de.prob.parser.ast.nodes.MachineNode;
 import de.prob.parser.ast.nodes.expression.ExprNode;
 import de.prob.parser.ast.nodes.expression.ExpressionOperatorNode;
+import de.prob.parser.ast.nodes.expression.IdentifierExprNode;
+import de.prob.parser.ast.nodes.expression.IfExpressionNode;
 import de.prob.parser.ast.nodes.expression.LambdaNode;
+import de.prob.parser.ast.nodes.expression.LetExpressionNode;
+import de.prob.parser.ast.nodes.expression.NumberNode;
+import de.prob.parser.ast.nodes.expression.QuantifiedExpressionNode;
+import de.prob.parser.ast.nodes.expression.RealNumberNode;
+import de.prob.parser.ast.nodes.expression.RecordFieldAccessNode;
+import de.prob.parser.ast.nodes.expression.RecordNode;
+import de.prob.parser.ast.nodes.expression.SetComprehensionNode;
+import de.prob.parser.ast.nodes.expression.StringNode;
+import de.prob.parser.ast.nodes.expression.StructNode;
+import de.prob.parser.ast.nodes.ltl.LTLBPredicateNode;
+import de.prob.parser.ast.nodes.ltl.LTLInfixOperatorNode;
+import de.prob.parser.ast.nodes.ltl.LTLKeywordNode;
+import de.prob.parser.ast.nodes.ltl.LTLPrefixOperatorNode;
+import de.prob.parser.ast.nodes.predicate.CastPredicateExpressionNode;
+import de.prob.parser.ast.nodes.predicate.IdentifierPredicateNode;
+import de.prob.parser.ast.nodes.predicate.IfPredicateNode;
+import de.prob.parser.ast.nodes.predicate.LetPredicateNode;
 import de.prob.parser.ast.nodes.predicate.PredicateNode;
 import de.prob.parser.ast.nodes.predicate.PredicateOperatorNode;
 import de.prob.parser.ast.nodes.predicate.PredicateOperatorWithExprArgsNode;
+import de.prob.parser.ast.nodes.predicate.QuantifiedPredicateNode;
+import de.prob.parser.ast.nodes.substitution.AnySubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.AssignSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.BecomesElementOfSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.BecomesSuchThatSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.ChoiceSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.ConditionSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.IfOrSelectSubstitutionsNode;
+import de.prob.parser.ast.nodes.substitution.LetSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.ListSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.OperationCallSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.SkipSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.VarSubstitutionNode;
+import de.prob.parser.ast.nodes.substitution.WhileSubstitutionNode;
+import de.prob.parser.ast.visitors.AbstractVisitor;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
 
@@ -16,7 +50,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class LambdaFunctionGenerator {
+public class LambdaFunctionGenerator implements AbstractVisitor<Void, Void> {
 
     private final STGroup group;
 
@@ -141,4 +175,190 @@ public class LambdaFunctionGenerator {
         return true;
     }
 
+    // TODO: Improve lambda implementation
+
+    @Override
+    public Void visitExprOperatorNode(ExpressionOperatorNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitIdentifierExprNode(IdentifierExprNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitCastPredicateExpressionNode(CastPredicateExpressionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitNumberNode(NumberNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitRealNumberNode(RealNumberNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitQuantifiedExpressionNode(QuantifiedExpressionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitSetComprehensionNode(SetComprehensionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLambdaNode(LambdaNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLetExpressionNode(LetExpressionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitIfExpressionNode(IfExpressionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitStringNode(StringNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitRecordNode(RecordNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitStructNode(StructNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitRecordFieldAccessNode(RecordFieldAccessNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLTLPrefixOperatorNode(LTLPrefixOperatorNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLTLKeywordNode(LTLKeywordNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLTLInfixOperatorNode(LTLInfixOperatorNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLTLBPredicateNode(LTLBPredicateNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitIdentifierPredicateNode(IdentifierPredicateNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitPredicateOperatorNode(PredicateOperatorNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitPredicateOperatorWithExprArgs(PredicateOperatorWithExprArgsNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitQuantifiedPredicateNode(QuantifiedPredicateNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLetPredicateNode(LetPredicateNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitIfPredicateNode(IfPredicateNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitVarSubstitutionNode(VarSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitWhileSubstitutionNode(WhileSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitListSubstitutionNode(ListSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitIfOrSelectSubstitutionsNode(IfOrSelectSubstitutionsNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitAssignSubstitutionNode(AssignSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitSkipSubstitutionNode(SkipSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitConditionSubstitutionNode(ConditionSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitAnySubstitution(AnySubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitLetSubstitution(LetSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitBecomesElementOfSubstitutionNode(BecomesElementOfSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitBecomesSuchThatSubstitutionNode(BecomesSuchThatSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitSubstitutionIdentifierCallNode(OperationCallSubstitutionNode node, Void expected) {
+        return null;
+    }
+
+    @Override
+    public Void visitChoiceSubstitutionNode(ChoiceSubstitutionNode node, Void expected) {
+        return null;
+    }
 }

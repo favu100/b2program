02a6a7ecf14b998a41c2ac4ea9bb79bf0eb887df
Fabian Vu <Fabian.Vu@hhu.de>
2024-10-25 21:05:30 +0200

    Allow using generic BObject in untyped BRelation and BSet
    Used for empty set equal to empty set etc.

--- btypes_big_integer/src/main/cpp/BObject.hpp
+++ btypes_big_integer/src/main/cpp/BObject.hpp
@@ -6,6 +6,11 @@ class BObject {
 
     public:
 
+	    typedef BTuple<S,T> current_type;
+	    typedef void value_type;
+        typedef S left_type;
+        typedef T right_type;
+
         friend bool operator !=(const BObject& p1, const BObject& p2) {return true;} // This is only used for untyped empty sets. This function is overridden in sub-classes.
 
         friend bool operator ==(const BObject& p1, const BObject& p2) {return false;} // This is only used for untyped empty sets. This function is overridden in sub-classes.
--- btypes_primitives/src/main/cpp/BObject.hpp
+++ btypes_primitives/src/main/cpp/BObject.hpp
@@ -6,6 +6,11 @@ class BObject {
 
     public:
 
+	    typedef BObject current_type;
+	    typedef void value_type;
+        typedef void left_type;
+        typedef void right_type;
+
         friend bool operator !=(const BObject& p1, const BObject& p2) {return true;} // This is only used for untyped empty sets. This function is overridden in sub-classes.
 
         friend bool operator ==(const BObject& p1, const BObject& p2) {return false;} // This is only used for untyped empty sets. This function is overridden in sub-classes.

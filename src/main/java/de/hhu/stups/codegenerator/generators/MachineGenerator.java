830eb80d39af3a219f05b508e84254d8cc64d83c
Fabian Vu <Fabian.Vu@hhu.de>
2024-10-26 15:34:00 +0200

    Try to fix an error

--- src/main/java/de/hhu/stups/codegenerator/generators/MachineGenerator.java
+++ src/main/java/de/hhu/stups/codegenerator/generators/MachineGenerator.java
@@ -445,10 +445,6 @@ public class MachineGenerator implements AbstractVisitor<String, Void> {
 		TemplateHandler.add(template, "parameters", declarationGenerator.generateDeclarations(parameters, OperationGenerator.DeclarationType.PARAMETER, false));
 		TemplateHandler.add(template, "assignments", assignments);
 
-		if(assignments.isEmpty()) {
-			return "";
-		}
-
 		return template.render();
 
 	}
